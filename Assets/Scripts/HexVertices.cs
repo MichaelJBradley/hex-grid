dd32ae7d5981dde39fbdd787a6a3851cb4836d13
Michael Bradley <mbrad94@gmail.com>
2020-12-26 17:20:14 -0800

    Add helper classes for drawing Hexes

---
+++ Assets/Scripts/HexVertices.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// A list of Hex types supported by this package.
+/// </summary>
+public enum HexTypes
+{
+    FlatTop,
+    PointyTop
+}
+
+/// <summary>
+/// HexVertices defines arrays of Vector3s representing the vertices of Hexes.
+/// All vertices arrays should start with the center vertex and be 0 offset.
+/// </summary>
+public class HexVertices
+{
+    /// <summary>
+    /// The ratio to determine the z or x component of a vertex depending on
+    /// whether the Hex is flat topped or pointy topped, respectively.
+    /// It about equals 0.866025 and comes from sin(pi/3) or sin(60°).
+    /// </summary>
+    private static readonly float HalfSizeRatio = Mathf.Sqrt(3) / 2f;
+    
+    /// <summary>
+    /// Vertices representing a flat topped Hex with a size of 1. The first
+    /// vertex is the center at (0, 0, 0).
+    /// </summary>
+    public static readonly Vector3[] FlatTop = new Vector3[]
+    {
+        new Vector3(0, 0, 0),
+        new Vector3(1, 0, 0),
+        new Vector3(0.5f, 0, HalfSizeRatio),
+        new Vector3(-0.5f, 0, HalfSizeRatio),
+        new Vector3(-1f, 0, 0),
+        new Vector3(-0.5f, 0, -HalfSizeRatio), 
+        new Vector3(0.5f, 0, -HalfSizeRatio)
+    };
+    
+    /// <summary>
+    /// Vertices representing a pointy topped Hex with a size of 1. The first
+    /// vertex is the center at (0, 0, 0).
+    /// </summary>
+    public static readonly Vector3[] PointyTop = new Vector3[]
+    {
+        new Vector3(0, 0, 0), 
+        new Vector3(HalfSizeRatio, 0, 0.5f), 
+        new Vector3(0, 0, 1f), 
+        new Vector3(-HalfSizeRatio, 0, 0.5f), 
+        new Vector3(-HalfSizeRatio, 0, -0.5f), 
+        new Vector3(0, 0, -1f), 
+        new Vector3(HalfSizeRatio, 0, -0.5f)
+    };
+}
